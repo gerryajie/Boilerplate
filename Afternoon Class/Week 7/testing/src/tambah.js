e1d4506df558d2055ee914d5dca7588dd0868e65
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-11-19 14:57:41 +0700

    push testing

--- Afternoon Class/Week 7/testing/src/tambah.js
+++ Afternoon Class/Week 7/testing/src/tambah.js
@@ -1,8 +1,4 @@
 function tambah(a, b) {
-  if (!a && !b && a !== 0 && b !== 0) {
-    return "silahkan isi input";
-  }
-
   return a + b
 }
 
--- Afternoon Class/Week 7/testing/tests/tambah.test.js
+++ Afternoon Class/Week 7/testing/tests/tambah.test.js
@@ -1,4 +1,4 @@
-const tambah = require("../src/tambah")
+const tambah = require("./tambah")
 
 test("pertambahan bilangan 1 dan 2 sama dengan 3", () => {
   expect(tambah(1, 2)).toBe(3);
